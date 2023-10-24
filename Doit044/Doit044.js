1e5aaee92ea7cf95975c3a6bf4f08e350b672f60
gsong <rmsdlf1206@gmail.com>
2023-10-24 23:56:25 +0900

    gnl

---
+++ Doit044/Doit044.js
@@ -0,0 +1,15 @@
+//문제: Doit044
+//0. 문제 이해 :
+//1. 시간 복잡도 확인:
+//2. 적용 알고리즘 확인:
+//3. 맞는 자료구조 확인:
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+solution(input);
+function solution(input) {
+    function gcd(a, b) {
+        return b == 0n ? a : gcd(b, a % b);
+    }
+}
